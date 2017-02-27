d586959df558e5a56d4cd5ace3d6031011c1a925
Fatma Gamze Atay <Fatma Gamze Atay>
2017-02-27 22:26:42 +0300

    add: project template

---
+++ ExchangeTraking/app/src/main/java/com/gsoft/exchangetraking/ExchangeHttpClient.java
@@ -0,0 +1,16 @@
+package com.gsoft.exchangetraking;
+
+import android.os.AsyncTask;
+
+
+
+/**
+ * Created by Gamze on 27.02.2017.
+ */
+
+public class ExchangeHttpClient extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
